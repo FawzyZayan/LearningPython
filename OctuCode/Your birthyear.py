50600e6413a1e4f9813e0a6e8b2dd84defc85568
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-01-13 20:10:54 +0300

    Your birthyear

---
+++ OctuCode/Your birthyear.py
@@ -0,0 +1,4 @@
+two_digits = input("Please enter twwo digits : \n")
+first_digit = int(two_digits[0])
+second_digit = int(two_digits[1])
+print("The sum of your two digits is: \n", first_digit + second_digit) 
