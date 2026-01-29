3376cd2dac7955fa90418f1b6eb9b96028ed54e9
Fawzy Zayan <161637249+FawzyZayan@users.noreply.github.com>
2026-01-29 22:26:17 +0300

    Delete OctuCode directory

--- OctuCode/Seconds, minutes and hours.py
+++
@@ -1,5 +0,0 @@
-total_seconds = int(input("Enter the duration in seconds: "))
-hours = total_seconds // 3600
-minutes = (total_seconds % 3600) // 60
-seconds = total_seconds  % 60
-print("The duration is: ", hours, "hours,", minutes, "minutes and", seconds, "seconds")
--- OctuCode/Your Birthyear.py
+++
@@ -1,2 +0,0 @@
-int_age = int(input("How old are you: "))
-print(f"You were born in {2026 - int_age}")
