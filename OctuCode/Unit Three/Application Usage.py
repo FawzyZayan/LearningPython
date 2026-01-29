086649dc19af41e3a327e17c0dd0b17d87d0965d
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-01-29 22:33:56 +0300

    updates

--- OctuCode/Unit Three/Application Usage.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to my Application!")
-age = int(input("Enter your age: "))
-if age >= 12:
-  print("Good. You can use the Application!")
-else:
-  print("Sorry, you can't use the Application.")
