c9cd515322f42ddf259efd7e9afa03389c420619
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-01-29 23:05:04 +0300

    Unit Three

---
+++ OctuCode/Unit 3/Students Degrees.py
@@ -0,0 +1,9 @@
+degree = int(input("Enter your degree: "))
+if degree >= 90:
+  print("Very Good!")
+elif degree >= 75:
+  print("Good!")
+elif degree >= 50:
+  print("Try Harder!")
+else:
+  print("Bad!")
