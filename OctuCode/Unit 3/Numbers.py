6ba07121496af48c9c259ab1aca41ea0b38fcfa4
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-01-29 22:58:23 +0300

    Numbers

---
+++ OctuCode/Unit 3/Numbers.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number: "))
+if number > 0:
+  print("The number you have entered in a Positive")
+elif number < 0:
+  print("The number you have enteres is Negative")
+else:
+  print("The number you have entered is Zero")
