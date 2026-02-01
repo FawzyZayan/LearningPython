03012cbe5ebaabf93cb373ae8460adbc9c0974f6
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-02-01 08:41:39 +0300

    Unit 3

---
+++ OctuCode/Unit 3/Chairs Game.py
@@ -0,0 +1,5 @@
+chair_number = int(input("Enter your Chair Number: "))
+if chair_number == 21:
+  print("Congratulations you won! The number was 21")
+else:
+  print(f"Oops you lost! The number was not {chair_number}. Try Again")
