daf4b00081eeed9c3cc1cef703e62043506a4cd2
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-02-09 22:11:51 +0300

    Unit 4

---
+++ OctuCode/Unit 4/Password guessing game.py
@@ -0,0 +1,16 @@
+import random
+
+laptop_random_number = random.randint(1000, 9999)
+print(laptop_random_number)
+
+user_password_guessing = int(input("Enter a 4-digit PIN password: "))
+
+if len(str(user_password_guessing)) > 4 or len(str(user_password_guessing)) < 4:
+  print("Please enter a 4-digit PIN password")
+  
+elif user_password_guessing == laptop_random_number:
+  print("Success! The PIN password matched")
+  
+else:
+  print("Failure! The PIN password didn't match")
+  print(f"The laptop generated this PIN: {laptop_random_number}")
