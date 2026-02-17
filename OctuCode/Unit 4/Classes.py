98bd6aefa53b8a6824b9fde8fd0b4400890ffe63
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-02-17 22:57:56 +0300

    Unit 4

--- OctuCode/Unit 4/Classes.py
+++ OctuCode/Unit 4/Classes.py
@@ -1,6 +1,20 @@
 class_a = [ "Fawzy", "Ali", "Mohammed", "AbdElGaleel", "ALHabib" ]
+
 class_b = [ "Youssef", "Ahmed", "Adam", "Yehia", "Yazen" ]
+all_students = []
 
-class_a.extend(class_b)
+all_students.extend(class_a)
 
-print(class_a)
+all_students.extend(class_b)
+
+print(all_students)
+
+print("\n")
+
+print("____________________________________________________________________________________________________")
+
+print("\n")
+
+students = class_a + class_b
+
+print(students)
