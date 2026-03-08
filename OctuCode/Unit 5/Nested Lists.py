60d11463df49f8647fd999153e6e63fcaa000654
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-03-09 02:30:50 +0300

    Unit 5

---
+++ OctuCode/Unit 5/Nested Lists.py
@@ -0,0 +1,11 @@
+import time
+
+basket = [ [ "Apples", "Bananas", "Oranges", "Berries", "Strawberry" ], [ "Water", "Milk", "Coffee", "Tea", "Juice" ] ]
+
+basket.extend([ "Candy", "Chips", "Cake", "Cookies", "Donuts" ])
+
+print(basket)
+
+time.sleep(2)
+
+print("____________________________________________________________________________________________")
