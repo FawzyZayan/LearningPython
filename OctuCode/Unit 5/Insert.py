17087ee44d6c422e13a9877f502d9a70dc621c5f
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-03-09 02:42:15 +0300

    Unit 5

--- OctuCode/Unit 5/Insert.py
+++ OctuCode/Unit 5/Insert.py
@@ -3,4 +3,10 @@ books = [ "Book 2", "Book 3", "Book 5" ]
 books.insert(0, "Book 1")
 books.insert(3, "Book 4")
 
+# To add Book 6 I can make like this:
+# books.insert(5, "Book 6")
+# Or I can make like this:
+# books.append("Book 6")
+# Because Book 6 is the last element in the list
+
 print(books)
