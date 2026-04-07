060af9b07272b6f869cba7498ca2060a1b09a0fb
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-04-07 21:19:08 +0300

    Unit 6

---
+++ OctuCode/Unit 6/Short Hand Operations.py
@@ -0,0 +1,38 @@
+import time
+
+a = 5
+a += 4
+print(f"5 + 4 = {a}")
+
+print("------")
+
+time.sleep(1)
+
+s = 10
+s -= 6
+print(f"10 - 6 = {4}")
+
+print("------")
+
+time.sleep(1)
+
+m = 9
+m *= 3
+print(f"9 x 3 = {m}")
+
+print("------")
+
+time.sleep(1)
+
+d = 46
+d /= 2
+print(f"46 / 2 = {23}")
+
+print("--------------------------")
+
+time.sleep(3)
+
+message = "Hello"
+message += " World!\n"
+message *= 3
+print(message)
