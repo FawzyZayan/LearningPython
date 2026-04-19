a215666c5b645aefbe399f476bf7c29aaf01542d
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-04-19 21:36:41 +0300

    Unit 6

--- OctuCode/Unit 6/The Flipped Sentence.py
+++ OctuCode/Unit 6/The Flipped Sentence.py
@@ -2,4 +2,4 @@ sentence = input("Enter a Scentence: ")
 words = sentence.split()
 flipped_words = words[::-1]
 flipped_scentence = " ".join(flipped_words)
-print(f"Flipped Scentence: "{flipped_scentence})
+print(f"Flipped Scentence: {flipped_scentence}")
