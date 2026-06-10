445bb8c510ad3c06a965f19336bc57508eea3f64
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-10 19:46:55 +0300

    Unit 9

--- OctuCode/Unit 9/Decoding.py
+++ OctuCode/Unit 9/Decoding.py
@@ -3,19 +3,19 @@ import string
 alphabet = string.ascii_lowercase
 message = input("Please enter a Message: ")
 shift_number = int(input("Enter a Shift Number: "))
-original_message = ""
+decrypted_message = ""
 
 for letter in message:
     if letter.lower() in alphabet:
-        original_position = alphabet.index(letter.lower())
-        new_position = (original_position - shift_number) % 26
-        original_letter = alphabet[new_position]
+        decrypted_position = alphabet.index(letter.lower())
+        new_position = (decrypted_position - shift_number) % 26
+        decrypted_letter = alphabet[new_position]
 
         if letter.isupper():
-            original_letter = original_letter.upper()
-        original_message += original_letter
+            decrypted_letter = decrypted_letter.upper()
+        decrypted_message += decrypted_letter
     else:
-        original_message += letter
+        decrypted_message += letter
 
 
-print(f"Here is the Original Message: {original_message}")
+print(f"Here is the Original Message: {decrypted_message}")
