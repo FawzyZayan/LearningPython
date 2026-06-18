d3a0831ac37410431016051011316aecbbcd657d
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-18 22:13:47 +0300

    Level 1 / Unit 10

---
+++ OctuCode/Level 1/Unit 10/Egypt Squad.py
@@ -0,0 +1,19 @@
+egypt_squad = {
+  10: "Mohammed Salah",
+  25: "Zizo",
+  1: "Mohamed El Shenawy",
+  7: "Trézéguet",
+  14: "Hamdy Fathy",
+}
+
+egypt_squad[23] = "Mostafa Shobeir"
+egypt_squad[22] = "Omar Marmoush"
+egypt_squad[5] = "Ramy Rabia"
+egypt_squad[8] = "Emam Ashour"
+
+for player in egypt_squad:
+  print("Player's Number:")
+  print(player)
+  print("Player's Name:")
+  print(egypt_squad[player])
+  print("____________________")
