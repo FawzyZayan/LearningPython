f7567d9c5038d2cca14c07d2d737faaebe67a4bc
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-18 22:22:55 +0300

    Level 1 / Unit 10

---
+++ OctuCode/Level 1/Unit 10/Dictionary Input.py
@@ -0,0 +1,10 @@
+personal_information = {}
+
+country = input("Where are you from: ")
+age = int(input("How old are you: "))
+
+personal_information["Name"] = name = input("What is your name: ")
+personal_information["Country"] = country
+personal_information["Age"] = age
+
+print(personal_information)
