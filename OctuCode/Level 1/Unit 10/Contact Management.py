e7967c03808c5ef891f80807dc0c1044c0b7a50b
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-20 17:35:20 +0300

    Level 1 / Unit 10

---
+++ OctuCode/Level 1/Unit 10/Contact Management.py
@@ -0,0 +1,59 @@
+contacts = {}
+
+print("Contact Management\n")
+user_choice = int(input("""
+1- Add Contact
+2- View Contact
+3- Edit a Contact
+4- Exit
+
+Please choose a nuber from 1 to 4: """))
+
+while user_choice == 1 or user_choice == 2 or user_choice == 3:
+  if user_choice == 1:
+    contacts["ID"] = ID = input("Enter the contact's ID: ")
+    contacts["name"] = name = input("Enter the contact's name: ")
+    contacts["number"] = number = int(input("Enter the contact's number: "))
+    
+    print(f"\n{name} was added successfully...\n")
+    print("Contact Management\n")
+    user_choice = int(input("""
+    1- Add Contact
+    2- View Contact
+    3- Edit a Contact
+    4- Exit
+
+    Please choose a nuber from 1 to 4: """))
+    continue
+  
+  elif user_choice == 2:
+    print(contacts)
+    print("Contact Management\n")
+    user_choice = int(input("""
+    1- Add Contact
+    2- View Contact
+    3- Edit a Contact
+    4- Exit
+
+    Please choose a nuber from 1 to 4: """))
+    continue
+  
+  elif user_choice == 3:
+    contacts["ID"] = ID = input("Enter the contact's ID: ")
+    contacts["name"] = name = input("Enter the contact's name: ")
+    contacts["number"] = number = int(input("Enter the contact's number: "))
+    print("\nSuccess...\n")
+    print("Contact Management\n")
+    user_choice = int(input("""
+    1- Add Contact
+    2- View Contact
+    3- Edit a Contact
+    4- Exit
+
+    Please choose a nuber from 1 to 4: """))
+    continue
+  
+  else:
+    print("Please run the program again and enter a number from 1 to 4")
+    
+print("Exiting the Program...")
