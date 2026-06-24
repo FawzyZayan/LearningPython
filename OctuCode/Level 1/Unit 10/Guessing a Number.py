0a9e303fb01e71fd9f49501c5befea7f6bd6deb0
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-24 21:33:53 +0300

    Level 1 / Unit 10

---
+++ OctuCode/Level 1/Unit 10/Guessing a Number.py
@@ -0,0 +1,20 @@
+import random
+import os
+
+def clear_screen():
+  if os.name == "nt":
+    os.system("cls")
+  else:
+    os.system("clear")
+
+random_number = random.randint(1, 10)
+
+while True:
+  clear_screen()
+  user_input = int(input("Guess a number between 1 and 10: "))
+  if user_input == random_number:
+    print("Congratulations! You guessed the correct number.")
+    break
+  else:
+    print("Wrong guess. Try again")
+    input("Press any key to continue...")
