fe55d6f6d3328058f5ded8053421dc5182fa2d12
Fawzy Zayan <fawzy.mzayan@gmail.com>
2026-06-24 22:00:31 +0300

    Level 1 / Unit 10

---
+++ OctuCode/Level 1/Unit 10/If statements shortcuts.py
@@ -0,0 +1,8 @@
+choice = input("Are you hungry? y/n: ").lower()
+
+# if choice == "y":
+#   print("Hungry")
+# else:
+#   print("Not Hungry")
+
+print("Hungry") if choice == "y" else print("Not Hungry")
